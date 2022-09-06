2f018c0781e7f0340e71b15d01532d3b2c274fe2
Subekshya Kayastha <subekshyakayastha4@gmail.com>
2022-09-06 12:19:28 +0545

    program game

--- a.py
+++ a.py
@@ -5,14 +5,14 @@ It can represent a multi-line comment
 We can write any thing here 
 '''
 
-a = 5 #here, a is a variable which stores/holds integer type value i.e 5
-naam = "Subekshya" #here, naam is a variable which stores/holds string type i.e "Subekshya"
+#a = 5 #here, a is a variable which stores/holds integer type value i.e 5
+#naam = "Subekshya" #here, naam is a variable which stores/holds string type i.e "Subekshya"
 
 # And in python string are always inside single or double quotes 
 # For example: "subekshya"  OR  "subekshya"
 
 #calling function with their name
-duita_num_joda()
+#duita_num_joda()
 
 #taking input from user:
 # my_name=input("Please enter your name-->: ")
